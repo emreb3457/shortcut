91a453a496a3e97b32546113006d28e817efb45a
Emre Başkaya <emrebaskayaaa@gmail.com>
2022-08-26 17:16:06 +0300

    server start

---
+++ back-end/app.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const app = express()
+const morgan = require("morgan")
+const cors = require("cors");
+const bodyparser = require("body-parser");
+require("dotenv").config({ path: "config/.env" })
+
+if (process.env.node_env) {
+    app.use(morgan("dev"));
+}
+app.use(cors());
+app.use(express.json());
+app.use(bodyparser.urlencoded({ extended: true }));
+app.use(bodyparser.json());
+
+
+
+//routes
+const userRoute = require("./routes/userRoute")
+
+app.use("/",userRoute)
+
+
+module.exports = app;
